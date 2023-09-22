9449849675c42c6b4cfb8bc4075385403ab2f6d1
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2023-09-22 15:46:32 +0300

    small typo

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 db.sequelize.authenticate()
     .then(() => {
-        console.log('Connection to database has been established successfully.');
+        console.log('Connection to the database has been established successfully.');
         return db.sequelize.sync();
     }).catch((error) => console.error('Unable to connect to the database:', error));
 
