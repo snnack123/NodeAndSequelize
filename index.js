5cb13cd869f982aae0ead035bf193ffe2de76f75
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2023-09-22 12:34:52 +0300

    modified login & register user routes

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const app = require('./server.js');
 const jwt = require('jsonwebtoken');
 const { verifyToken } = require('./utils/middlewares.js');
 const db = require('./app/models/index.js');
+const bcrypt = require('bcrypt');
 
 const Product = db.Product;
 
 const PORT = process.env.PORT || 3000;
+const saltRounds = process.env.BCRYPT_SALT ? Number(process.env.BCRYPT_SALT) : 10;
 
 app.get("/", (req, res) => {
     res.send("I will be shown on the Browser");
@@ -13,25 +15,33 @@ app.get("/", (req, res) => {
     // console.log("I will be shown on the Terminal");
 });
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     try {
-        const { username, password } = req.body;
-
-        if (!username || !password) {
-            res.status(400).send('Username and Password required');
-            return;
+        if (!req.body.email || !req.body.password) {
+            return res.status(400).json({ error: 'Missing required fields' });
         }
 
-        if (!process.env.JWT_SECRET || !process.env.JWT_EXPIRES_IN) {
-            throw new Error('Missing JWT_SECRET or JWT_EXPIRES_IN in environment variables');
+        const user = await db.User.findOne({
+            where: { email: req.body.email }, 
+            attributes: { exclude: ['createdAt', 'updatedAt'] }
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
         }
 
-        const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ error: 'Invalid password' });
+        }
 
-        res.status(200).json({ token });
+        delete user.dataValues.password;
+
+        const token = jwt.sign({ username: user.name }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_IN });
+
+        res.status(200).json({ message: 'Login successful', user: user, token: token });
     } catch (error) {
-        console.log(error);
-        res.status(500).send(error);
+        res.status(500).json({ error: error.message });
     }
 });
 
@@ -46,5 +56,26 @@ app.post('/products', verifyToken, async (req, res) => {
 
     res.sendStatus(200);
 });
+
+app.post('/users', async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.email || !req.body.password) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }	
+
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+
+        const newUser = await db.User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 
 app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
